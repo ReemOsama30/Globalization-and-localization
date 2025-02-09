db3ab7c8f2bcfaec066cab6e5a443e533e3c86da
ReemOsama30 <reem.osama3052001@gmail.com>
2025-02-09 23:39:15 +0200

    Add project files.

---
+++ Globalization-and-localization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Globalization_and_localization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
